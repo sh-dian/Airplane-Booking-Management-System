9273d9f0468619070ab82ae0396f6bd18af304c5
dian <sh.alqudree@gmail.com>
2021-12-30 23:11:45 +0800

    update team member name

--- Airplane Booking Management System/src/AirplaneSystem/Mas.java
+++ Airplane Booking Management System/src/AirplaneSystem/Mas.java
@@ -4,10 +4,14 @@
  */
 package AirplaneSystem;
 
-/**
- *
- * @author User
- */
+/** 
+ * @Group 8 
+ * 1. SHARIFAH LYDIEANNA BT SYED SHAMSUDDIN (CB20050)
+ * 2. NURUL ATIKAH BT FADZIL HALMI (CB20133)
+ * 3. AHMAD HILMAN BIN AHMAD BADRUDDIN (CB20093)
+ * 4. WAN MUHAMMAD DZULKHAIRIE BIN WAN ZAHARI (CB20096)
+ * 5. HANIS SYAFIQA BT KHAIRIL AZLI (CB20106)
+ */ 
 public class Mas extends Airplane implements AirplaneDetail{
     private int AirplaneCode;
     
